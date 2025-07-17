f9b5aa9c99bcf79cf549515d56ce60dd62be0537
linhvodoiss <tangbang250820@gmail.com>
2025-07-17 23:59:26 +0700

    fix bug

--- src/app/_components/headerMail/index.tsx
+++ src/app/_components/headerMail/index.tsx
@@ -14,13 +14,16 @@ const menuItems = [
 export default function HeaderMail() {
   return (
     <WebHeaderStyled className='bg-background-primary border-primary-system border-b-2'>
-      <div className='header__container mx-auto flex w-full max-w-[1920px] items-center justify-between pr-8 text-xl text-white'>
+      <div className='mx-auto flex w-full max-w-[1920px] items-center justify-between pr-8 text-xl text-white'>
         <div className='flex items-center justify-start gap-8'>
           <Image src='/images/logo.png' alt='logo' width={1024} height={1024} className='w-24 object-contain' />
           <ul className='flex items-center justify-center'>
             {menuItems.map(item => (
               <li key={item.label}>
-                <Link href={item.href} className='header__link'>
+                <Link
+                  href={item.href}
+                  className='relative cursor-pointer px-8 py-6 font-bold text-white no-underline after:absolute after:bottom-3 after:left-1/2 after:h-0.5 after:w-0 after:-translate-x-1/2 after:bg-white after:transition-all after:duration-300 hover:after:w-2/3'
+                >
                   {item.label}
                 </Link>
               </li>
